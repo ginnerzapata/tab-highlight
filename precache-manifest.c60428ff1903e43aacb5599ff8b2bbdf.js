db4ae77197cec0a80695d23ba205131fc90e1307
Ginner Zapata <ginnerzapata@gmail.com>
2020-12-21 09:39:17 -0300

    Updates

---
+++ precache-manifest.c60428ff1903e43aacb5599ff8b2bbdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05197b3c23abac3e05b88bfe9e1efc3",
+    "url": "/tab-highlight/index.html"
+  },
+  {
+    "revision": "5673f5ab5e613b1e267a",
+    "url": "/tab-highlight/static/css/main.9fd141e0.chunk.css"
+  },
+  {
+    "revision": "87930bda40e181d403d1",
+    "url": "/tab-highlight/static/js/2.8cd4dda0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/tab-highlight/static/js/2.8cd4dda0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5673f5ab5e613b1e267a",
+    "url": "/tab-highlight/static/js/main.0d537a8b.chunk.js"
+  },
+  {
+    "revision": "aff5b0d1ae863d8712a8",
+    "url": "/tab-highlight/static/js/runtime-main.4e8a6bfa.js"
+  }
+]);
